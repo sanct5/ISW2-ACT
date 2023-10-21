590f8684f2f63b8e0fafdd025428d47dd15d9854
JhulyVivas <131303835+JhulyVivas@users.noreply.github.com>
2023-10-21 10:47:24 -0500

    Schema

--- src/models/Process.mjs
+++ src/models/Process.mjs
@@ -1,17 +1,60 @@
 import { Schema, model } from 'mongoose';
-import { TYPE_OF_FILTERS } from '../commons/constans.mjs';
+import { TYPE_OF_FILTERS, FILTERS_STATUSES } from '../commons/constans.mjs';
 
-const ProcessSchema = new Schema({
-  filters: {
-    type: [
-      {
+const FilterSchema = new Schema(
+  {
+    name: {
+      type: String,
+      enum: TYPE_OF_FILTERS,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: FILTERS_STATUSES,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: false,
+    },
+  },
+  { _id: true },
+);
+
+const ImageSchema = new Schema(
+  {
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    filters: {
+      type: [FilterSchema],
+      required: true,
+    },
+  },
+  { _id: true },
+);
+
+const ProcessSchema = new Schema(
+  {
+    filters: {
+      type: [{
         type: String,
         enum: TYPE_OF_FILTERS,
         required: true,
-      },
-    ],
+      }],
+    },
+    images: {
+      type: [ImageSchema],
+      required: true,
+    },
   },
-}, { timestamps: true });
+  { timestamps: true },
+);
 
 const ProcessModel = model('process', ProcessSchema);
 
